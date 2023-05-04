9a74f9e4b6edc94cf905be16f149157ca6f8eb40
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-05-04 12:43:12 -0600

    Updated table relationships in db

--- server/models.py
+++ server/models.py
@@ -11,13 +11,38 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+
+    animals = db.relationship("Animal", back_populates="zookeeper")
+
+    def __repr__(self):
+        return f'<Zookeeper {self.name}, {self.birthday}>'
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+
+    animals = db.relationship("Animal", back_populates="enclosure")
+
+    def __repr__(self):
+        return f'<Enclosure {self.name}, {self.environment}, {self.open_to_visitors}>'
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey("zookeepers.id"))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey("enclosures.id"))
+
+    enclosure = db.relationship("Enclosure", back_populates="animals")
+    zookeeper = db.relationship("Zookeeper", back_populates="animals")
+
+    def __repr__(self):
+        return f'<Animal {self.name}, {self.species}>'
